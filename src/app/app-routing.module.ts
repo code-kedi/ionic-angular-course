787bccbeb2b86377588bf3fd23db64faa6a27288
Sonja Peschke <sonjape91@gmail.com>
2025-04-12 19:06:46 +0200

    Add recipe detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,31 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'recipes',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
-{
-  path: 'recipes',
-  loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesPageModule)
-}
+  {
+    path: 'recipes',
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./recipes/recipes.module').then((m) => m.RecipesPageModule),
+      },
+      {
+        path: ':recipeId',
+        loadChildren: () =>
+          import('./recipes/recipe-detail/recipe-detail.module').then(
+            (m) => m.RecipeDetailPageModule
+          ),
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.page.html',
+  styleUrls: ['./recipe-detail.page.scss'],
+  standalone: false,
+})
+export class RecipeDetailPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
